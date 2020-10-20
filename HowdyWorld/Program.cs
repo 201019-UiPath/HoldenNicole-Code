f4b28b055301558ac581e402945089ddf4f86ae6
Nicole Holden <nicole.holden@revature.net>
2020-10-20 17:07:18 -0500

    Howdy World

---
+++ HowdyWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HowdyWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+            int year;
+            Console.WriteLine("Howdy! Please enter your first name here: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine($"Awesome {firstName}, now we need your last name: ");
+            lastName=Console.ReadLine();
+            Console.WriteLine($"Nice to meet you {firstName} {lastName} always good to meet another trainee!");
+        }
+    }
+}
