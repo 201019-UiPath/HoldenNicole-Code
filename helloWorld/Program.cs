82b4880d87987f5a510d140f088e891a3f3fee88
Nicole Holden <nicole.holden@revature.net>
2020-10-19 21:08:50 -0500

    first commit

---
+++ helloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿//1.0 predefined namespace
+using System;
+
+//1.1 custom defined namespace
+namespace helloWorld
+{
+    class Program
+    {
+        //execution starts here
+            //comandline arguments
+        static void Main(string[] args)
+        {
+            string firstname, lastname;
+            Console.WriteLine("Hello, please enter your firstname");
+            firstname=Console.ReadLine();
+            Console.WriteLine("Hello, please enter your lastname");
+            lastname=Console.ReadLine();
+            Console.WriteLine($"Welcome to Revature {firstname} {lastname}");
+        }
+    }
+}
